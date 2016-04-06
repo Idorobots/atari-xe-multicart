19c1b37830927f95484185039f6a2fb37e28bd48
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-04-06 07:02:44 +0200

    Implemented bulk of the firmware logic.

--- src/menu.c
+++ src/menu.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <conio.h>
 #include <atari.h>
+#include <unistd.h>
 
-extern char _dos_type;                          /* bss variable */
+#define CCTL ((unsigned char *) 0xD500)
+#define CART_START 0xBFFA
+#define COPY_AREA 0x8000
 
-int main(void) {
+void __fastcall__ menu(void) {
   unsigned char off = 0;
+  unsigned int *address = (unsigned int *) 0xBFFA;
 
   clrscr();
-  printf("Select game (1-32): ");
+  printf("    _  _            _  __  _____ \n");
+  printf("   /_\\| |_ __ _ _ _(_) \\ \\/ / __|\n");
+  printf("  / _ \\  _/ _` | '_| |  >  <| _| \n");
+  printf(" /_/ \\_\\__\\__,_|_| |_| /_/\\_\\___|\n");
+  printf("  __  __      _ _   _             _   \n");
+  printf(" |  \\/  |_  _| | |_(_)__ __ _ _ _| |_ \n");
+  printf(" | |\\/| | || | |  _| / _/ _` | '_|  _|\n");
+  printf(" |_|  |_|\\_,_|_|\\__|_\\__\\__,_|_|  \\__|\n\n");
 
-  if (_dos_type != 1) scanf("%d", &off);
-
-  // TODO Copy this program into the RAM.
-
-  printf("You may now [RESET] your console.");
+  // TODO Needs a better selection menu.
+  printf("Select a game (1-32): ");
+  scanf("%d", &off);
 
   // Swap the game.
-  *((unsigned char *) 0xD500) = off & 0x3F; // NOTE At most 6 bits should be used.
+  *CCTL = off;
 
-  // TODO Jump into the newly swapped game.
+  // Jump into the newly swapped game.
+  if(off > 0) {
+    __asm__ ("jmp (%w)", CART_START);
+  }
+
+  printf("Bye!\n");
+  sleep(1);
+}
+
+int main(void) {
+  // Backup menu() into the second cartridge slot not to loose it later.
+  memcpy((void *) COPY_AREA, (const void *) menu, 1024); // FIXME Compute the size somehow.
+
+  // Jump into the backup.
+  __asm__ ("jmp %w", COPY_AREA);
 
   return 0;
 }
